8d407e110bcdb78c56f5491d7c7dfc39e061c600
cemil <cemilyilmaz.be@gmail.com>
2020-12-07 14:22:07 +0100

    horaire fini attend style

--- resto.js
+++ resto.js
@@ -1,75 +1,95 @@
 document.querySelector('.main').innerHTML=''
-const mainContainer = document.querySelector('.main');
+const main = document.querySelector('.main');
 
 let weekday = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
 
 const horaire = [
     { //Lundi
         jour: weekday[1],
-        hoursstart1: 10,
-        hoursclose1: 15,
-        hoursstart2: 18,
-        hoursclose2: 23,
+        hours_Start1: 10,
+        hours_Close1: 15,
+        hours_Start2: 18,
+        hours_Close2: 23,
     },
     { //Mardi
         jour: weekday[2],
-        hoursstart1: 10,
-        hoursclose1: 15,
-        hoursstart2: 18,
-        hoursclose2: 23,
+        hours_Start1: 10,
+        hours_Close1: 15,
+        hours_Start2: 18,
+        hours_Close2: 23,
     },
     { //Mercredi
         jour: weekday[3],
-        hoursstart1: 10,
-        hoursclose1: 15,
-        hoursstart2: 18,
-        hoursclose2: 23,
+        hours_Start1: 10,
+        hours_Close1: 15,
+        hours_Start2: 18,
+        hours_Close2: 23,
     },
     { //Jeudi
         jour: weekday[4],
-        hoursstart1: 10,
-        hoursclose1: 15,
-        hoursstart2: 18,
-        hoursclose2: 23,
+        hours_Start1: 10,
+        hours_Close1: 15,
+        hours_Start2: 18,
+        hours_Close2: 23,
     },
     { //Vendredi
         jour: weekday[5],
-        hoursstart1: 10,
-        hoursclose1: 15,
-        hoursstart2: 18,
-        hoursclose2: 23,
+        hours_Start1: 10,
+        hours_Close1: 15,
+        hours_Start2: 18,
+        hours_Close2: 23,
     },
     { //Samedi
         jour: weekday[6],
-        hoursstart: 12,
-        hoursclose: 23,
+        hours_Start: 12,
+        hours_Close: 23,
     },
     { //Dimanche
         jour: weekday[0],
-        hoursstart: 12,
-        hoursclose: 23,
+        hours_Start: 12,
+        hours_Close: 23,
     },
 ]
 
 let today = new Date
 
 for (let element of horaire) {
+    const alljour = document.createElement('div')
     if ( weekday[6] == weekday[today.getDay()] || weekday[0] == weekday[today.getDay()]) {
         let heure = today.getHours();
-        if (heure >= element.hoursstart && heure <= element.hoursclose) {
-            console.log('green');
+        if (heure >= element.hours_Start && heure <= element.hours_Close) {
+            alljour.style = 'background-color: green;'
         }
         else {
-            console.log('red');
+            alljour.style = 'color: red;'
         }
     }
     else if (element.jour == weekday[today.getDay()] ) {
         let heure = today.getHours();
-        if (heure >= element.hoursstart1 && heure <= element.hoursclose1 || heure >= element.hoursstart2 && heure <= element.hoursclose2) {
-            console.log('green');
+        if (heure >= element.hours_Start1 && heure < element.hours_Close1 || heure >= element.hours_Start2 && heure < element.hours_Close2) {
+            alljour.style = 'background-color: green;'
         }
         else {
-            console.log('red');
+            alljour.style = 'color: red;'
         }
+    }
+    main.appendChild(alljour)
+
+    const jour = document.createElement('p')
+    jour.textContent = element.jour
+    alljour.appendChild(jour)
+
+    if (element.jour == 'Sat' || element.jour == 'Sun') {
+        const heureDebut = document.createElement('p')
+        heureDebut.textContent = element.hours_Start +":00 "+ element.hours_Close +":00"
+        alljour.appendChild(heureDebut)
+    }
+    else{
+        const heureDebut = document.createElement('p')
+        heureDebut.textContent = element.hours_Start1 +":00 "+ element.hours_Close1 +":00"
+        alljour.appendChild(heureDebut)
+        const heureFin = document.createElement('p')
+        heureFin.textContent = element.hours_Start2 +":00 "+ element.hours_Close2 +":00"
+        alljour.appendChild(heureFin)
     }
 }

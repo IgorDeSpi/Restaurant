eb00171f79e30495cb685f89db6893e459bf2f25
Abdelilah Zaidi <abdelilahzaidi1983@gmail.com>
2020-12-08 10:11:05 +0100

    second commit

--- restauantab/main.js
+++ restauantab/main.js
@@ -1,6 +1,6 @@
 
-let weekday = [ 'Samedi', 'Lundi', 'Mardi', 
-'Mercredi', 'Jeudi', 'Vendredi', 'Dimanche' ];
+let weekday = [ 'Dimanche', 'Lundi', 'Mardi', 
+'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ];
 
 const horaire = [
     { //Lundi
@@ -122,3 +122,17 @@ for (let element of horaire) {
     
   
 }
+
+document.getElementById('recherche').addEventListener('keyup', function(e) {
+  var recherche = this.value.toLowerCase();
+  var documents = document.querySelectorAll('.document');
+ 
+  Array.prototype.forEach.call(documents, function(document) {
+    // On a bien trouvé les termes de recherche.
+    if (document.innerHTML.toLowerCase().indexOf(recherche) > -1) {
+      document.style.display = 'block';
+    } else {
+      document.style.display = 'none';
+    }
+  });
+});
